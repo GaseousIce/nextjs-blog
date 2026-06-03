205ce44a8fcaac5ac5b7cd5ebdbf1227c6f6bb8a
waturrr <104217734+GaseousIce@users.noreply.github.com>
2026-06-03 16:14:44 +0000

    feat: enhance layout with header, footer, and global styles

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,13 @@
 const nextConfig = {
     allowedDevOrigins: ["127.0.0.1"],
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "s4.anilist.co",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
